1867dc855facf5918ef6a06d2f167daa482cf858
Dalbert1 <dylan.m.albert1@gmail.com>
2020-06-27 16:54:46 -0500

    Added create_secret.yaml

--- ig-ansible/rds_scripts/configure_rds.sh
+++ ig-ansible/rds_scripts/configure_rds.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$#" != "1" ]; then
+if [ "$#" != "2" ]; then
 	sudo yum update -y
 	sudo yum install -y git
 	sudo yum install -y python3
@@ -12,25 +12,19 @@ if [ "$#" != "1" ]; then
 	sudo pip3 install boto3
 	cd /home/ec2-user
 	git clone https://github.com/Dalbert1/python-image-gallery.git
+	git clone https://github.com/Dalbert1/ig_ansible_boot.git
 	chown -R ec2-user:ec2-user python-image-gallery
 
-# PASS IN PARAMETERS FOR IMAGE GALLERY DB
-echo "
-create user image_gallery login password 'simple';
-grant image_gallery to postgres;
-create database image_gallery owner image_gallery;
-grant image_gallery to image_gallery;
-\q
-" >> create_ig_user.sql
+	#export POSTGRES_HOST="some-host.com" # $1 arg 1
+	#export POSTGRES_PORT="5432" # $2 arg 1
+	#export POSTGRES_DATABASE="some_database" # $3 arg 1
+	#export POSTGRES_USERNAME="some_user" # $4 arg 1
+	#export POSTGRES_PASSWORD="some_password" # $5 arg 1
 
-echo "
-create table users (username varchar(100) primary key, password varchar(100), full_name varchar(200));
-insert into users values ('fred','foo','Fred Flinstone');
-insert into users values ('Dalb','Al','D');
-\q
-" >> create_users_tbl.sql
-
-	#psql -h $1 -U postgres -d image_gallery -f ./rds_scripts/create_ig_user.sql
+	cd ig-ansible
+	
+	export PGPASSWORD=$2;
+	psql -h $1 -U postgres -f ./rds_scripts/create_ig_user.sql
 	#psql -h $1 -U image_gallery -d image_gallery -f ./rds_scripts/create_users_tbl.sql
 	exit 1
 fi
