c524595f23a934ed4a13caec8b5e3bf0a0ff5484
Dalbert1 <dylan.m.albert1@gmail.com>
2020-06-28 00:31:38 -0500

    Modified RDS script

--- ig-ansible/rds_scripts/configure_rds.sh
+++ ig-ansible/rds_scripts/configure_rds.sh
@@ -1,36 +1,34 @@
 #!/bin/bash
-
-if [ "$#" != "1" ]; then
-	sudo yum update -y
-	sudo yum install -y git
-	sudo yum install -y python3
-	sudo yum install -y postgresql
-	sudo yum install -y gcc
-	sudo yum install -y python3-devel
-	sudo yum install -y postgresql-devel
-	sudo yum install -y jq # json parser
-	sudo pip3 install psycopg2
-	sudo pip3 install boto3
-	cd /home/ec2-user
-	pip3 install --user boto3
-	pip3 install --user psycopg2
-	git clone https://github.com/Dalbert1/python-image-gallery.git
-	git clone https://github.com/Dalbert1/ig_ansible_boot.git
-	chown -R ec2-user:ec2-user python-image-gallery
-	chown -R ec2-user:ec2-user ig_ansible_boot
-	cd /ig_ansible_boot/ig-ansible
-	export AWS_DEFAULT_REGION=us-east-2
-	export AWS_DEFAULT_OUTPUT=json
-	export PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id sec-ig-postgres | jq --raw-output .SecretString | jq -r ."password")
-	export PGUSER=$(aws secretsmanager get-secret-value --secret-id sec-ig-postgres | jq --raw-output .SecretString | jq -r ."username")
-	export PGHOST=$(aws secretsmanager get-secret-value --secret-id sec-ig-postgres | jq --raw-output .SecretString | jq -r ."host")
-	psql  -f ./rds_scripts/create_ig_user.sql
-	export PGDATABASE=$(aws secretsmanager get-secret-value --secret-id sec-ig-image_gallery | jq --raw-output .SecretString | jq -r ."database_name")
-	export PGUSER=$(aws secretsmanager get-secret-value --secret-id sec-ig-image_gallery | jq --raw-output .SecretString | jq -r ."username")
-	export PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id sec-ig-image_gallery | jq --raw-output .SecretString | jq -r ."password")
-	psql -f ./rds_scripts/create_users_tbl.sql
-	exit 1
-fi
+sudo yum update -y
+sudo yum install -y git
+sudo yum install -y python3
+sudo yum install -y postgresql
+sudo yum install -y gcc
+sudo yum install -y python3-devel
+sudo yum install -y postgresql-devel
+sudo yum install -y jq # json parser
+sudo pip3 install psycopg2
+sudo pip3 install boto3
+cd /home/ec2-user
+pip3 install --user boto3
+pip3 install --user psycopg2
+git clone https://github.com/Dalbert1/python-image-gallery.git
+git clone https://github.com/Dalbert1/ig_ansible_boot.git
+chown -R ec2-user:ec2-user python-image-gallery
+chown -R ec2-user:ec2-user ig_ansible_boot
+cd /home/ec2-user/ig_ansible_boot/ig-ansible
+export AWS_DEFAULT_REGION=us-east-2
+export AWS_DEFAULT_OUTPUT=json
+export PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id sec-ig-postgres | jq --raw-output .SecretString | jq -r ."password")
+export PGUSER=$(aws secretsmanager get-secret-value --secret-id sec-ig-postgres | jq --raw-output .SecretString | jq -r ."username")
+export PGHOST=$(aws secretsmanager get-secret-value --secret-id sec-ig-postgres | jq --raw-output .SecretString | jq -r ."host")
+psql  -f ./rds_scripts/create_ig_user.sql
+export PGDATABASE=$(aws secretsmanager get-secret-value --secret-id sec-ig-image_gallery | jq --raw-output .SecretString | jq -r ."database_name")
+export PGUSER=$(aws secretsmanager get-secret-value --secret-id sec-ig-image_gallery | jq --raw-output .SecretString | jq -r ."username")
+export PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id sec-ig-image_gallery | jq --raw-output .SecretString | jq -r ."password")
+psql -f ./rds_scripts/create_users_tbl.sql
+exit 1
 
 
-# CAN GET RANDOM PASSWORDS WITH AWS CLI
+# CAN GET RANDOM PASSWORDS WITH AWS CLI OR ANSIBLE
+#/var/log/boot.log  ---  System boot log
